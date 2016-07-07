b472c69b1e8c77f72622a3951b008aeea338edc8
akinori-kawamitsu <kawamitsu.akinori@gmail.com>
2016-07-07 22:06:35 +0900

    togglemenu、scss修正

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,8 +1,8 @@
 jQuery(function($){
 $(document).ready(function(){
   $('.toggle').click(function(){
-	$(this).next('ul').toggleClass('visible');
-	$(this).toggleClass('visible');
+	$(this).next('ul').toggleClass('dep-visible');
+	$(this).toggleClass('dep-visible');
  });
 });
 });
